7b7202fa341048370ceafd89f49de50a4894fdfc
Josh Spratt <jspratt02@icloud.com>
2025-11-30 11:28:30 -0600

    Tweaks (#5)
    * 2024 day 2
    
    * Add helper

---
+++ aoc_helpers/input_reader.py
@@ -0,0 +1,16 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class AocInputReader:
+    def __init__(self, file_path):
+        self.file_path = file_path
+
+    def read_input_to_str(self):
+        logger.info("Reading input: %s", self.file_path)
+        with open(self.file_path, "rb") as file:
+            raw = file.read()
+        logger.info("Read %d bytes from %s", len(raw), self.file_path)
+        data = raw.decode("utf-8").strip()
+        return data
