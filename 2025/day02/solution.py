43d1ad6d4987b5b3f7fb94b45543baba31c21662
Josh Spratt <jspratt02@icloud.com>
2025-12-02 00:01:51 -0600

    Drying things up a little bit (#9)

--- 2025/day02/solution.py
+++ 2025/day02/solution.py
@@ -7,7 +7,6 @@ logger = logging.getLogger(__name__)
 
 def split_product_id_ranges(product_id_data: str) -> list[str]:
     """Take the raw data and create a list of ranges (as strings)"""
-    logger.info("Splitting product ID ranges into a list")
     return [x.strip() for x in product_id_data.split(",")]
 
 
@@ -29,25 +28,18 @@ def split_product_id_and_check_for_repetition(product_id: str) -> bool:
     return False
 
 
-def part1_sum_invalid_ids(product_id_ranges: list[str]) -> int:
+def sum_invalid_ids(product_id_ranges: list[str], part_number: int) -> int:
     logger.info("Summing invalid product IDs")
     invalid_id_sum = 0
     for product_id_range in product_id_ranges:
         product_id_list = build_product_id_list(product_id_range)
         for product_id in product_id_list:
-            if split_product_id_and_check_for_repetition(product_id):
-                invalid_id_sum += int(product_id)
-    return invalid_id_sum
-
-
-def part2_sum_invalid_ids(product_id_ranges: list[str]) -> int:
-    logger.info("Summing invalid product IDs")
-    invalid_id_sum = 0
-    for product_id_range in product_id_ranges:
-        product_id_list = build_product_id_list(product_id_range)
-        for product_id in product_id_list:
-            if bool(re.fullmatch(r"(.+)\1+", product_id)):
-                invalid_id_sum += int(product_id)
+            if part_number == 1:
+                if split_product_id_and_check_for_repetition(product_id):
+                    invalid_id_sum += int(product_id)
+            elif part_number == 2:
+                if bool(re.fullmatch(r"(.+)\1+", product_id)):
+                    invalid_id_sum += int(product_id)
     return invalid_id_sum
 
 
@@ -60,9 +52,9 @@ def main():
     input_reader = AocInputReader("2025/day02/input.txt")
     input_str = input_reader.read_input_to_str()
     product_id_ranges = split_product_id_ranges(input_str)
-    part1_invalid_id_sum = part1_sum_invalid_ids(product_id_ranges)
+    part1_invalid_id_sum = sum_invalid_ids(product_id_ranges, 1)
     logger.info("Part 1 Solution -- The sum of invalid IDs is %s", part1_invalid_id_sum)
-    part2_invalid_id_sum = part2_sum_invalid_ids(product_id_ranges)
+    part2_invalid_id_sum = sum_invalid_ids(product_id_ranges, 2)
     logger.info("Part 2 Solution -- The sum of invalid IDs is %s", part2_invalid_id_sum)
 
 
